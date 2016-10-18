b7c937a30b830f91386b5a721f48df8f1858a2b8
János Zsolt <zsoca8711@gmail.com>
2016-10-18 22:19:50 +0200

    3. gyakorlat feltöltve

---
+++ Gyakorlat03/0_Feladat.sql
@@ -0,0 +1,94 @@
+/**
+* Tranzakció kezelés
+**/
+
+CREATE TABLE library_gyak3 (
+    library_id SERIAL,
+    library_name text NOT NULL UNIQUE,
+    CONSTRAINT library_gy3_pk PRIMARY KEY (library_id)
+);
+
+INSERT INTO library_gyak3(library_name) VALUES ('test1');
+INSERT INTO library_gyak3(library_name) VALUES ('test2');
+select * from library_gyak3;
+
+/* Ezen a ponton van egy kis táblánk */
+
+/* ------ 1 - DDL-tranzakció ------ **/
+
+BEGIN;
+
+DROP TABLE IF EXISTS library_gyak3;
+
+CREATE TABLE library_gyak3 (
+    library_id SERIAL,
+    library_name text NOT NULL UNIQUE,
+    library_postcode integer CHECK (library_postcode > 1000),
+    CONSTRAINT library_gy3_pk PRIMARY KEY (library_id)
+);
+
+ALTER TABLE library_gyak3 ADD COLUMN library_director text NOT NULL;
+
+select * from library_gyak3;
+ROLLBACK;
+
+select * from library_gyak3;
+
+/* Visszaállt minden a régi kerékvágásba */
+
+BEGIN;
+
+DROP TABLE IF EXISTS library_gyak3;
+
+CREATE TABLE library_gyak3 (
+    library_id SERIAL,
+    library_name text NOT NULL UNIQUE,
+    library_postcode integer CHECK (library_postcode > 1000),
+    CONSTRAINT library_gy3_pk PRIMARY KEY (library_id)
+);
+
+ALTER TABLE library_gyak3 ADD COLUMN library_director text NOT NULL;
+
+select * from library_gyak3;
+COMMIT;
+
+select * from library_gyak3;
+
+/* ------ 2 - DML-tranzakció ------ **/
+
+BEGIN;
+
+INSERT INTO library_gyak3(library_name,library_postcode,library_director) VALUES ('teszt3',3123,'Nincs');
+INSERT INTO library_gyak3(library_name,library_postcode,library_director) VALUES ('teszt4',6667,'Nincs');
+INSERT INTO library_gyak3(library_name,library_postcode,library_director) VALUES ('teszt5',8641,'Nincs');
+INSERT INTO library_gyak3(library_name,library_postcode,library_director) VALUES ('nemteszt10',8341,'Nincs');
+UPDATE library_gyak3 SET library_director = 'A Vezető';
+
+COMMIT;
+
+select * from library_gyak3;
+
+BEGIN;
+
+INSERT INTO library_gyak3(library_name,library_postcode,library_director) VALUES ('teszt6',1414,'A Vezető');
+INSERT INTO library_gyak3(library_name,library_postcode,library_director) VALUES ('teszt7',4141,'A Vezető');
+INSERT INTO library_gyak3(library_name,library_postcode,library_director) VALUES ('teszt8',8510,'A Vezető');
+UPDATE library_gyak3 SET library_director = 'A Vezető';
+
+select * from library_gyak3;
+
+ROLLBACK;
+
+select * from library_gyak3;
+
+/** Tranzakcióba egy parancsot is tehetünk persze... */
+
+BEGIN;
+DELETE FROM library_gyak3 WHERE library_name LIKE 'teszt%';
+ROLLBACK;
+
+/** Nézzük meg tranzakció nélkül **/
+
+DELETE FROM library_gyak3 WHERE library_name LIKE 'teszt%';
+ROLLBACK;
+select * from library_gyak3;
---
+++ Gyakorlat03/1_Feladat.sql
@@ -0,0 +1,31 @@
+CREATE TABLE book_data
+(
+  ean text NOT NULL,
+  author text,
+  title text,
+  weight numeric(6,2),
+  weight_unit text,
+  CONSTRAINT bd_pk PRIMARY KEY (ean)
+);
+
+COPY book_data FROM '/tmp/book.data.csv'; /**itt írd át az elérési utat a sajátodra **/
+
+select count(*) from book_data;
+
+select count(*) from book_data where weight is null;
+
+/** Hozzuk le azokat a könyveket, amelyek olyan szerzőkhöz tartoznak, akik többször szerepelnek az adathalmazban **/
+WITH duplicated_authors  AS (
+  SELECT author FROM book_data
+  GROUP BY author
+  HAVING count(*) > 1
+)
+SELECT book_data.*  FROM book_data
+INNER JOIN duplicated_authors ON duplicated_authors.author = book_data.author;
+
+/** Írjuk ki COPY-val egy CSV fájlba azokat a könyveket (a tömeggel), amelyek súlya nagyobb, mint az átlagos tömeg. **/
+COPY (
+SELECT book_data.ean,book_data.weight  FROM book_data
+WHERE weight > (select avg(weight) from book_data)
+ORDER BY weight DESC
+) TO '/tmp/heaviest.books.csv';
